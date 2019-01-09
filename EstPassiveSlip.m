50b8a88dab5f74db2b821544679f44527b0933c9
kimhiroshiedu <hiroshi@windows>
2019-01-09 20:31:16 +0900

    revise

--- EstPassiveSlip.m
+++ EstPassiveSlip.m
@@ -7,7 +7,7 @@ input.optfile='./PARAMETER/opt_bound_par.txt';
 % READ PARAMETER FOR MCMC Inversion 
 [prm]=Read_Parameters(input);
 % READ OBSERVATION FILE
-[obs]=Read_obs(prm.file_obs);
+[obs]=Read_Obs(prm.file_obs);
 % READ BLOCK BOUNDARY FILE in DIRECTORY
 [BLK,obs]=READ_BLOCK_BOUND(prm.dir_block,obs);
 % READ BLOCK INTERFACE BOUNDARY in DIRECTORY 
@@ -24,6 +24,11 @@ SHOW_BLOCK_BOUND(BLK)
 [TRI,obs]=GREEN_TRI(BLK,obs);
 % Combain to Green function
 [D,G]=COMB_GREEN(BLK,obs,TRI);
+% Calculate Green's function of displacement
+[gu]=MakeGreenDisp(xyz,trixyz3);
+% Calculate Green's function of strain
+[gs]=MakeGreenStrain(trixyzC,trixyz3,sitaS,sitaD,normVec);
+
 % CALC. ABIC AND BLOCK MOTION
 [BLK,obs]=CALC_AIC(BLK,obs,POL,prm);
 % BLOCK MOTION BETWEEN TWO BLOCKS
@@ -105,7 +110,7 @@ fprintf('==================\n')
 disp('PASS READ_PARAMETERS')
 end
 %% Read observation data
-function obs=Read_obs(file_obs)
+function obs=Read_Obs(file_obs)
 %-------------------
 % INPUT format Observations:
 % unit is mm/yr
@@ -140,4 +145,92 @@ obs(1).lonmin=min(obs(1).alon);
 fprintf('==================\n') 
 fprintf('Number of GNSS site %4d \n',n)
 fprintf('==================\n') 
+end
+%% make Green's function of displacement
+function [gu]=MakeGreenDisp(xyz,trixyz3)
+% This function make Green's function of displacement (gu).
+% Input
+%  xyz     : site location.
+%  trixyz3 : trimesh coordianate.
+% Output
+%  gu.st   : surface displacement due to strike slip on a fault.
+%  gu.dp   : surface displacement due to dip slip on a fault.
+%  gu.ts   : surface displacement due to tensile slip on a fault.
+%  *Each matrix (gu.*) contain 3 x NOBS by NFLT elements.
+
+pr  =0.25; % Poisson's ratio
+nobs=size(xyz,1);
+nflt=size(trixyz3,1);
+sx  =xyz(:,1);
+sy  =xyz(:,2);
+sz  =zeros(nobs,1);
+
+for n=1:nflt
+    trix=[trixyz3(n,1),trixyz3(n,4),trixyz3(n,7)];
+    triy=[trixyz3(n,2),trixyz3(n,5),trixyz3(n,8)];
+    triz=[trixyz3(n,3),trixyz3(n,6),trixyz3(n,9)];
+%     [U] = CalcTriDisps(sx, sy, sz, x, y, z, pr, ss, ts, ds);
+    [U] = CalcTriDisps(sx, sy, sz, trix, triy, triz, pr, 1, 0, 0); % Strike
+    gu.st(1:3:3*nobs,n)= U.x;
+    gu.st(2:3:3*nobs,n)= U.y;
+    gu.st(3:3:3*nobs,n)=-U.z;
+    [U] = CalcTriDisps(sx, sy, sz, trix, triy, triz, pr, 0, 1, 0); % Tensile
+    gu.ts(1:3:3*nobs,n)= U.x;
+    gu.ts(2:3:3*nobs,n)= U.y;
+    gu.ts(3:3:3*nobs,n)=-U.z;
+    [U] = CalcTriDisps(sx, sy, sz, trix, triy, triz, pr, 0, 0, 1); % Dip
+    gu.dp(1:3:3*nobs,n)= U.x;
+    gu.dp(2:3:3*nobs,n)= U.y;
+    gu.dp(3:3:3*nobs,n)=-U.z;
+end
+
+end
+%% make Green's function of strain
+function [gs]=MakeGreenStrain(trixyzC,trixyz3,sitaS,sitaD,normVec)
+% This function make Green's function of strain (gs).
+% Input
+%  trixyzC : center of trimesh.
+%  trixyz3 : trimesh coordianate.
+%  sitaS   : angle of fault strike from X-axis.
+%  sitaD   : angle of fault dip from XY-plane.
+%  normVec : normalized normal vectors of faults.
+% Output
+%  gs.st   : strain due to strike slip on a fault.
+%  gs.dp   : strain due to dip slip on a fault.
+%  gs.ts   : strain due to tensile slip on a fault.
+%  *Each matrix (gs.*) contain 6 x NFLT by NFLT elements.
+pr  =0.25; % Poisson's ratio
+nflt=size(trixyz3,1);
+sx=trixyzC(:,1)+normVec(:,1);
+sy=trixyzC(:,2)+normVec(:,2);
+sz=trixyzC(:,3)+normVec(:,3);
+
+for n=1:nflt
+    trix=[trixyz3(n,1),trixyz3(n,4),trixyz3(n,7)];
+    triy=[trixyz3(n,2),trixyz3(n,5),trixyz3(n,8)];
+    triz=[trixyz3(n,3),trixyz3(n,6),trixyz3(n,9)];
+%     [S] = CalcTriStrains(sx, sy, sz, x, y, z, pr, ss, ts, ds);
+    [S] = CalcTriStrains(sx, sy, sz, trix, triy, triz, pr, 1, 0, 0); % Strike
+    gs.st(1:6:6*nflt,n)=S.xx;
+    gs.st(2:6:6*nflt,n)=S.xy;
+    gs.st(3:6:6*nflt,n)=S.xz;
+    gs.st(4:6:6*nflt,n)=S.yy;
+    gs.st(5:6:6*nflt,n)=S.yz;
+    gs.st(6:6:6*nflt,n)=S.zz;
+    [S] = CalcTriStrains(sx, sy, sz, trix, triy, triz, pr, 0, 1, 0); % Tensile
+    gs.ts(1:6:6*nflt,n)=S.xx;
+    gs.ts(2:6:6*nflt,n)=S.xy;
+    gs.ts(3:6:6*nflt,n)=S.xz;
+    gs.ts(4:6:6*nflt,n)=S.yy;
+    gs.ts(5:6:6*nflt,n)=S.yz;
+    gs.ts(6:6:6*nflt,n)=S.zz;
+    [S] = CalcTriStrains(sx, sy, sz, trix, triy, triz, pr, 0, 0, 1); % Dip
+    gs.dp(1:6:6*nflt,n)=S.xx;
+    gs.dp(2:6:6*nflt,n)=S.xy;
+    gs.dp(3:6:6*nflt,n)=S.xz;
+    gs.dp(4:6:6*nflt,n)=S.yy;
+    gs.dp(5:6:6*nflt,n)=S.yz;
+    gs.dp(6:6:6*nflt,n)=S.zz;
+end
+[gs]=trans_xyz2strdip(gs,sitaS,sitaD);
 end
