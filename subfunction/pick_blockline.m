1cbebe4a15734f62b5fd1f9a63e6680b11fef1af
hiroshi <hiroshi@mint>
2019-09-19 17:36:09 +0900

    Update pick_blockline.m

--- subfunction/pick_blockline.m
+++ subfunction/pick_blockline.m
@@ -1,5 +1,8 @@
-load('D:\MasterResearch\GMT\data\faults_jb1.txt')
-load('D:\MasterResearch\inversion\PassiveSlip\Result_win\Test_09\blk.mat')
+% load('D:\MasterResearch\GMT\data\faults_jb1.txt')
+% load('D:\MasterResearch\inversion\PassiveSlip\Result_win\Test_09\blk.mat')
+load('~/MasterResearch/GMT/data/faults_jb1.txt')
+load('~/MasterResearch/inversion/PassiveSlip/Result_red/Test_08/blk.mat')
+load('~/MasterResearch/inversion/MCMC_Estslip/Result_red/Test_58/BLK.mat')
 
 figure(10); clf(10)
 % Coast line
@@ -15,28 +18,39 @@ ax.YLim = [ 32  48]; % NE Japan
 % ax.YLim = [ 30  37]; % SW Japan
 hold on
 
-% JB1 block line
-alon1 = faults_jb1(:,2);
-alat1 = faults_jb1(:,3);
-alon2 = faults_jb1(:,4);
-alat2 = faults_jb1(:,5);
-alon = [alon1; alon2];
-alat = [alat1; alat2];
-for nf = 1:size(faults_jb1,1)
-  lon = faults_jb1(nf,[2,4]);
-  lat = faults_jb1(nf,[3,5]);
-  plot(lon,lat, 'k'); hold on
-  plot(lon,lat,'xk'); hold on
+% % 1. JB1 block line
+% alon1 = faults_jb1(:,2);
+% alat1 = faults_jb1(:,3);
+% alon2 = faults_jb1(:,4);
+% alat2 = faults_jb1(:,5);
+% alon = [alon1; alon2];
+% alat = [alat1; alat2];
+% for nf = 1:size(faults_jb1,1)
+%   lon = faults_jb1(nf,[2,4]);
+%   lat = faults_jb1(nf,[3,5]);
+%   plot(lon,lat, 'k'); hold on
+%   plot(lon,lat,'xk'); hold on
+% end
+
+% 1. Kimura block line (SW Japan)
+load('~/MasterResearch/inversion/MCMC_Estslip/Result_red/Test_58/BLK.mat')
+alon = [];
+alat = [];
+for nb = 1:size(blk,2)
+alon = [alon; BLK(nb).LON];
+alat = [alat; BLK(nb).LAT];
+plot(BLK(nb).LON,BLK(nb).LAT, 'm'); hold on
+plot(BLK(nb).LON,BLK(nb).LAT,'xm'); hold on
 end
 
-% Kimura block line
+% 2. Kimura block line (NE Japan)
 blon = [];
 blat = [];
 for nb = 1:size(blk,2)
   blon = [blon; blk(nb).lon];
   blat = [blat; blk(nb).lat];
-  plot(blk(nb).lon,blk(nb).lat, 'r'); hold on
-  plot(blk(nb).lon,blk(nb).lat,'xr'); hold on
+  plot(blk(nb).lon,blk(nb).lat, 'c'); hold on
+  plot(blk(nb).lon,blk(nb).lat,'xc'); hold on
 end
 
 % Pick points
@@ -92,3 +106,34 @@ Newlat = [Newlat; newlat];
 
 % Final plot
 plot(Newlon, Newlat, 'g')
+
+
+function temp_block
+load('Kuril_Forearc_block.txt')
+load('NEJapan_block.txt')
+load('NEJapan_backarc_block.txt')
+load('Niigata_block.txt')
+load('Okhotsk_block.txt')
+
+plot(Kuril_Forearc_block(:,1),Kuril_Forearc_block(:,2), 'r')
+plot(Kuril_Forearc_block(:,1),Kuril_Forearc_block(:,2),'xr')
+plot(NEJapan_block(:,1),NEJapan_block(:,2), 'r')
+plot(NEJapan_block(:,1),NEJapan_block(:,2),'xr')
+plot(NEJapan_backarc_block(:,1),NEJapan_backarc_block(:,2), 'r')
+plot(NEJapan_backarc_block(:,1),NEJapan_backarc_block(:,2),'xr')
+plot(Niigata_block(:,1),Niigata_block(:,2), 'r')
+plot(Niigata_block(:,1),Niigata_block(:,2),'xr')
+plot(Okhotsk_block(:,1),Okhotsk_block(:,2), 'r')
+plot(Okhotsk_block(:,1),Okhotsk_block(:,2),'xr')
+
+blon = [blon; Kuril_Forearc_block(:,1)];
+blat = [blat; Kuril_Forearc_block(:,2)];
+blon = [blon; NEJapan_block(:,1)];
+blat = [blat; NEJapan_block(:,2)];
+blon = [blon; NEJapan_backarc_block(:,1)];
+blat = [blat; NEJapan_backarc_block(:,2)];
+blon = [blon; Niigata_block(:,1)];
+blat = [blat; Niigata_block(:,2)];
+blon = [blon; Okhotsk_block(:,1)];
+blat = [blat; Okhotsk_block(:,2)];
+end
