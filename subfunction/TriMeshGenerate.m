fb1561023447af5cc5ce124d187fcac522808e03
hiroshi <hiroshi@win>
2019-11-13 17:40:59 +0900

    Update TriMeshGenerate.m

--- subfunction/TriMeshGenerate.m
+++ subfunction/TriMeshGenerate.m
@@ -1,26 +1,38 @@
-function TriMeshGenerate
+function TriMeshGenerate(model)
 % This script generate tri_*.txt file of GMT format
 % Coded by Hiroshi Kimura 2019/11/08
 figure(10); clf(10)
-GeneratePACmesh_iwa;
-GeneratePHSmesh_iwa;
-GeneratePHSsagamimesh_iwa;
+GeneratePACmesh(model);
+GeneratePHSmesh(model);
+GeneratePHSsagamimesh(model);
 hold off
 end
 
 %% Iwasaki model
 % PHS Sagami
-function GeneratePHSsagamimesh_iwa
-plate_iwasaki_phssagami;
-if ispc
-    phs = load('/MasterResearch/plate_data/plate_iwasaki/PHS_Plate/phs_regional/phs_2015_5a_r_2017.xyz');
-else
-    phs = load('~/MasterResearch/plate_data/plate_iwasaki/PHS_Plate/phs_regional/phs_2015_5a_r_2017.xyz');
+function GeneratePHSsagamimesh(model)
+switch model
+    case 'iwasaki'
+        plate_iwasaki_phssagami;
+        if ispc
+            phs = load('/MasterResearch/plate_data/plate_iwasaki/PHS_Plate/phs_regional/phs_2015_5a_r_2017.xyz');
+        else
+            phs = load('~/MasterResearch/plate_data/plate_iwasaki/PHS_Plate/phs_regional/phs_2015_5a_r_2017.xyz');
+        end
+    case 'hirose'
+        plate_hirose_phssagami;
+        if ispc
+            phs = load('/MasterResearch/plate_data/plate_hirose/combined_contour_swjp/phs_hirose_slab1_combine.xyz');
+        else
+            phs = load('~/MasterResearch/plate_data/plate_hirose/combined_contour_swjp/phs_hirose_slab1_combine.xyz');
+        end
+    otherwise
+        fprintf('Not applicable.\n'); return
 end
 F = scatteredInterpolant(phs(:,1),phs(:,2),phs(:,3));
 msh.POS(:,3) = F(msh.POS(:,1),msh.POS(:,2));
 
-file = 'Meshes/model_iwasaki/tri_phssagami.txt';
+file = ['Meshes/model_',model,'/tri_phssagami.txt'];
 savetri(file,msh);
 
 figure(10)
@@ -30,17 +42,30 @@ hold on
 end
 
 % PHS Izu to Ryukyu
-function GeneratePHSmesh_iwa
-plate_iwasaki_phs;
-if ispc
-    phs = load('/MasterResearch/plate_data/plate_iwasaki/PHS_Plate/phs_regional/phs_2015_5a_r_2017.xyz');
-else
-    phs = load('~/MasterResearch/plate_data/plate_iwasaki/PHS_Plate/phs_regional/phs_2015_5a_r_2017.xyz');
+function GeneratePHSmesh(model)
+switch model
+    case 'iwasaki'
+        plate_iwasaki_phs;
+        if ispc
+            phs = load('/MasterResearch/plate_data/plate_iwasaki/PHS_Plate/phs_regional/phs_2015_5a_r_2017.xyz');
+        else
+            phs = load('~/MasterResearch/plate_data/plate_iwasaki/PHS_Plate/phs_regional/phs_2015_5a_r_2017.xyz');
+        end
+    case 'hirose'
+        plate_hirose_phs;
+        if ispc
+            phs = load('/MasterResearch/plate_data/plate_hirose/combined_contour_swjp/phs_hirose_slab1_combine.xyz');
+        else
+            phs = load('~/MasterResearch/plate_data/plate_hirose/combined_contour_swjp/phs_hirose_slab1_combine.xyz');
+        end
+    otherwise
+        fprintf('Not applicable.\n'); return
 end
+
 F = scatteredInterpolant(phs(:,1),phs(:,2),phs(:,3));
 msh.POS(:,3) = F(msh.POS(:,1),msh.POS(:,2));
 
-file = 'Meshes/model_iwasaki/tri_phs.txt';
+file = ['Meshes/model_',model,'/tri_phs.txt'];
 savetri(file,msh);
 
 figure(10)
@@ -50,17 +75,30 @@ hold on
 end
 
 % PAC
-function GeneratePACmesh_iwa
-plate_iwasaki_pac;
-if ispc
-    pac = load('/MasterResearch/plate_data/plate_iwasaki/PAC_Plate/pac_regional/pac_2017_4a.xyz');
-else
-    pac = load('~/MasterResearch/plate_data/plate_iwasaki/PAC_Plate/pac_regional/pac_2017_4a.xyz');
+function GeneratePACmesh(model)
+switch model
+    case 'iwasaki'
+        plate_iwasaki_pac;
+        if ispc
+            pac = load('/MasterResearch/plate_data/plate_iwasaki/PAC_Plate/pac_regional/pac_2017_4a.xyz');
+        else
+            pac = load('~/MasterResearch/plate_data/plate_iwasaki/PAC_Plate/pac_regional/pac_2017_4a.xyz');
+        end
+    case 'hirose'
+        plate_hirose_pac;
+        if ispc
+            pac = load('/MasterResearch/plate_data/plate_hirose/PAC/pac_hirose_slab1_combine.xyz');
+        else
+            pac = load('~/MasterResearch/plate_data/plate_hirose/PAC/pac_hirose_slab1_combine.xyz');
+        end
+    otherwise
+        fprintf('Not applicable.\n'); return
 end
+
 F = scatteredInterpolant(pac(:,1),pac(:,2),pac(:,3));
 msh.POS(:,3) = F(msh.POS(:,1),msh.POS(:,2));
 
-file = 'Meshes/model_iwasaki/tri_pac.txt';
+file = ['Meshes/model_',model,'/tri_pac.txt'];
 savetri(file,msh);
 
 figure(10)
