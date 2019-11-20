68943432156fdc70d9c6ed1c1f409c444c335234
hiroshi <hiroshi@mint>
2019-11-20 19:27:31 +0900

    Update maid2ia.m

--- subfunction/maid2ia.m
+++ subfunction/maid2ia.m
@@ -1,7 +1,8 @@
 function maid2ia(folder)
 folder  = fullfile(pwd,folder);
 [input] = read_chafile(folder);
-rename_maid2ia(input);
+rename_maid2ia_cha(input);
+rename_maid2ia_tcha(folder)
 end
 
 function [input] = read_chafile(folder)
@@ -13,7 +14,7 @@ for ii = 1:nit
 end
 end
 
-function rename_maid2ia(input)
+function rename_maid2ia_cha(input)
 nit = size(input,2);
 for ii = 1:nit
   load(input(ii).fname); fprintf('load %s ...',input(ii).fname);
@@ -23,4 +24,18 @@ for ii = 1:nit
   cha = rmfield(cha,'maidcompress');
   save(input(ii).fname); fprintf('save %s\n',input(ii).fname);
 end
+end
+
+function rename_maid2ia_tcha(folder)
+file = fullfile(folder,'tcha.mat');
+load(file);
+tcha.aveaid  = tcha.aveaspid ;
+tcha.medaid  = tcha.medaspid ;
+tcha.stdaid  = tcha.stdaspid ;
+tcha.covaid  = tcha.covaspid ;
+tcha.coraid  = tcha.coraspid ;
+tcha.smpaid  = tcha.smpaspid ;
+tcha.ndataid = tcha.ndataspid;
+tcha = rmfield(tcha,{'aveaspid','medaspid','stdaspid','covaspid','coraspid','smpaspid','ndataspid'});
+save(file,'tcha','-v7.3');
 end
