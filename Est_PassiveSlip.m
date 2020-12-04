5735f473c9bf2a5de5dd836b98b56c44d3467b27
hkimu <hkimu@DESKTOP-G7O4R5K.local>
2020-12-04 19:45:02 +0900

    Update Est_PassiveSlip.m

--- Est_PassiveSlip.m
+++ Est_PassiveSlip.m
@@ -2234,8 +2234,8 @@ res.oldl  =   inf(   1,prm.nrep,precision);
 % pri.old   =   inf(   1,prm.nrep,precision);
 
 % Scale adjastment of rwd
-mcscale  = rwd * 5e-4;
-mascale  = rwd * 3e-1 .* (1e-2 .* repmat(blk(1).aline_dz,2,prm.nrep));
+mcscale  = rwd * 1e-2;
+mascale  = rwd * 3e-2 .* repmat(blk(1).aline_dz,2,prm.nrep);
 mpscale  = rwd * 1e-10 .* repmat(ones(mp.n,1,precision).*~eul.id,1,prm.nrep);
 miscale  = rwd * 1e-10;
 lascale  = rwd * 1e+0;
